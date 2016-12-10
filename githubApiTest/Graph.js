779d86c868366e14eb076d4fdf2076deb68d17d4
leon3110l <leon3110l@gmail.com>
2016-12-10 22:58:25 +0100

    dingen gedaan met github API

--- githubApiTest/Graph.js
+++ githubApiTest/Graph.js
@@ -4,9 +4,9 @@ function Graph(name, graphContainer) {
   this.width;
   this.makeNewElement = function() {
     document.getElementById("graphContainer"+graphContainer).innerHTML +="<div class='graphElement'><div id=graphElementBar"+graphAmount+" class='graphElementBar'><div class='graphElementName'>"+name+"</div></div></div>";
-    document.getElementById("graphElementBar"+graphAmount).style.width = this.width;
-    graphAmount++;
+    document.getElementById("graphElementBar"+graphAmount).style.width = this.width+"em";
   }
+  graphAmount++;
 }
 
 function makeGraphContainer(){
--- githubApiTest/main.js
+++ githubApiTest/main.js
@@ -1,4 +1,6 @@
-var repoRequest = new XMLHttpRequest;
+
+//it got too messy and it didn't work
+/*var repoRequest = new XMLHttpRequest;
 var statsRequest = [];
 var statsRequestData =[];
 var data;
@@ -12,7 +14,11 @@ repoRequest.onreadystatechange = function() {
     //console.log(data);
     for (var i = 0; i < data.length; i++) {
       statsRequest.push(new XMLHttpRequest);
+      elements.push(new Graph(data[i].name, 0));
+      elements[i].makeNewElement();
+      graphAmount++;
     }
+    graphAmount = 0;
     for (var i = 0; i < statsRequest.length; i++) {
       //console.log("in forloop");;
       statsRequest[i].onreadystatechange = function() {
@@ -24,7 +30,11 @@ repoRequest.onreadystatechange = function() {
               console.log(statsRequestData[j][k]);
               if (statsRequestData[j][k].author.login == "leon-4A6C") {
                 console.log(statsRequestData[j][k].total);
-                totalcommits.push(statsRequestData[j][k].total);
+                console.log(elements);
+                elements[k].width = statsRequestData[j][k].total+"em";
+                elements[k].update();
+                console.log(graphAmount);
+                graphAmount++;
               }
             }
           }
@@ -33,16 +43,60 @@ repoRequest.onreadystatechange = function() {
       statsRequest[i].open("GET", "https://api.github.com/repos/leon-4A6C/"+data[i].name+"/stats/contributors");
       statsRequest[i].send();
     }
-    for (var i = 0; i < data.length; i++) {
-      console.log(totalcommits);
-      elements.push(new Graph(data[i].name, 0));
-      for (var j = 6; j < 10; j++) {
-        console.log(totalcommits[j]);
-        elements[i].width = "10em";
-      }
-      elements[i].makeNewElement();
-    }
   }
 }
 repoRequest.open("GET","https://api.github.com/users/leon-4A6C/repos");
 repoRequest.send();
+*/
+
+makeGraphContainer();
+showStats();
+var repoData, statsData, parsedStatsData, elements;
+
+
+function repoRequest() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "https://api.github.com/users/leon-4A6C/repos",  false);
+  xmlhttp.send();
+  repoData = JSON.parse(xmlhttp.responseText);
+  return repoData;
+}
+
+function statsRequest() {
+  var data = repoRequest();
+  statsData = [];
+  //console.log(data);
+  //console.log(data[i].name);
+  for (var i = 0; i < data.length; i++) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", "https://api.github.com/repos/leon-4A6C/"+data[i].name+"/stats/contributors", false);
+    xmlhttp.send();
+    statsData.push(JSON.parse(xmlhttp.responseText));
+  }
+  return statsData;
+}
+
+function parseStats() {
+  parsedStatsData = [];
+  var data = statsRequest();
+  console.log(data);
+  for (var i = 0; i < data.length; i++) {
+    for (var j = 0; j < data[i].length; j++) {
+      if (data[i][j].author.login == "leon-4A6C" || data[i][j].author.login == "leon3110l") {
+        parsedStatsData.push(data[i][j].total);
+      }
+    }
+  }
+  console.log(parsedStatsData);
+  return parsedStatsData;
+}
+
+function showStats() {
+  var data = parseStats();
+  elements = [];
+  for (var i = 0; i < data.length; i++) {
+    elements.push(new Graph(repoData[i].name, 0));
+    elements[i].width = data[i];
+    elements[i].makeNewElement();
+  }
+}
